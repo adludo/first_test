3ff594b5e03953fa89716d9c23cd110cbec6d010
Alex <alex@alexs-air-8.home>
2020-04-27 01:04:21 +1000

    first test

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ import {
   Text,
   StatusBar,
   Button,
+  TextInput,
 } from 'react-native';
 
 import {
@@ -29,9 +30,16 @@ export default function App() {
   const [outputText, setOutputText] = useState('Open up App.js to start working on app');
   return (
     // <SafeAreaView>
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('The new text')} />
+    <View style={{padding: 50}}>
+      <View style = {{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput 
+          placeholder="Course Goal" 
+          style={{width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}/>
+        <Button title="Add"/>
+      </View>
+      <View>
+        
+      </View>      
     </View>
     //</SafeAreaView>
   );
