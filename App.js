d770938951f0816780d23c5c02a0380977dba07f
Alex <alex@alexs-air-8.home>
2020-05-03 02:13:25 +1000

    2:32:25 Next step delete

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ import {
   StatusBar,
   Button,
   TextInput,
+  FlatList,
 } from 'react-native';
 
 import {
@@ -26,34 +27,26 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
-  const [courseGoals, setCourseGoals] = useState([]);
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-  }
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
-  const addGoalHandler = () => {
-    console.log(enteredGoal);
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
-  }
+export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const addGoalHandler = goalTitle => {
+    console.log(goalTitle);
+    setCourseGoals(currentGoals => [...currentGoals,
+    { key: Math.random().toString(), value: goalTitle }]);
+  };
 
   return (
     <View style={styles.screen}>
-      <View>
-        <View style={styles.inputContainer}>
-          <TextInput
-            placeholder="Course Goal"
-            style={styles.input}
-            onChangeText={goalInputHandler}
-            value={enteredGoal}
-          />
-          <Button title="Add" onPress={addGoalHandler} />
-        </View>
-      </View>
-      <View>
-        {courseGoals.map((goal) => <Text> {goal}</Text>)}
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
+      <FlatList
+        data={courseGoals}
+        renderItem={itemData =>
+          <GoalItem title={itemData.item.value} />
+        } />
     </View>
   );
 }
@@ -61,58 +54,5 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 50,
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  input: {
-    width: '80%',
-    borderColor: 'black',
-    borderWidth: 1,
-    padding: 10
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
   },
 });
