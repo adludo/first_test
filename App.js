b0f4fa7999e256ea3cec67087351ba526b2dfda0
Alex <alex@alexs-air-8.home>
2020-05-04 01:29:44 +1000

    basic downpat, 3:41:29

--- App.js
+++ App.js
@@ -32,20 +32,43 @@ import GoalInput from './components/GoalInput';
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
   const addGoalHandler = goalTitle => {
+    if (goalTitle.length === 0) {
+      return;
+    }
     console.log(goalTitle);
     setCourseGoals(currentGoals => [...currentGoals,
-    { key: Math.random().toString(), value: goalTitle }]);
+    { id: Math.random().toString(), value: goalTitle }]);
+    setIsAddMode(false);
+  };
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  }
+
+  const cancelGoalAdditionHandler = () => {
+    setIsAddMode(false);
   };
 
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler} />
+      <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
+      <GoalInput
+        visible={isAddMode}
+        onAddGoal={addGoalHandler}
+        onCancel={cancelGoalAdditionHandler} />
       <FlatList
         data={courseGoals}
         renderItem={itemData =>
-          <GoalItem title={itemData.item.value} />
+          <GoalItem
+            id={itemData.item.id}
+            onDelete={removeGoalHandler}
+            title={itemData.item.value}
+          />
         } />
     </View>
   );
